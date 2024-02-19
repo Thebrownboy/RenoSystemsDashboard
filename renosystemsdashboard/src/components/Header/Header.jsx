e37eb1728a00e97a7d1c4bdd8347e9ca84ca0d0f
thebrownboy <abdallaxpmostafa@gmail.com>
2024-02-19 03:22:30 +0200

    customizing the header on small screens

--- renosystemsdashboard/src/components/Header/Header.jsx
+++ renosystemsdashboard/src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import burger from "../../assets/burger.svg";
 import { weekDays, months } from "../../../constants";
 import notificationIcon from "../../assets/notification.svg";
+import burgerIcon from "../../assets/burger.svg";
 function Header({ firstName, lastName, updateSideBarDisplay, sideBarDisplay }) {
   console.log(new Date().getDay());
   const date = new Date();
+  const [showSettings, updateShowSettings] = useState(false);
   return (
     <div className="border-b ">
       <header className="flex items-center justify-between bg-white  px-3 py-2">
@@ -36,7 +38,7 @@ function Header({ firstName, lastName, updateSideBarDisplay, sideBarDisplay }) {
           </div>
         </div>
 
-        <div className="user-settings  flex gap-3  items-center">
+        <div className="hidden  sm:flex text-sm md:text-[1rem] user-settings   gap-3  items-center">
           <p className=" question text-[gray] border-1 border border-[gray] w-8 rounded-full flex justify-center font-bold">
             ?
           </p>
@@ -56,6 +58,38 @@ function Header({ firstName, lastName, updateSideBarDisplay, sideBarDisplay }) {
             {firstName.substring(0, 1)}
             {lastName.substring(0, 1)}
           </div>
+        </div>
+        <div className="sm:hidden relative ">
+          <div
+            className="cursor-pointer"
+            onClick={() => updateShowSettings(!showSettings)}
+          >
+            <img src={burgerIcon} width={16} height={16} alt="" />
+          </div>
+
+          {showSettings && (
+            <div className=" absolute left-[-64px] top-7 rounded-sm flex  flex-col gap-3 justify-center items-center bg-[#f1eaea] p-5">
+              <p className=" question text-[gray] border-1 border border-[gray] w-8 rounded-full flex justify-center font-bold">
+                ?
+              </p>
+
+              <div
+                className="notification  px-3 relative  before:w-4 before:h-4 before:bg-red-600 before:absolute
+           before:text-[white] before:text-[10px] before:content-['+9'] before:top-0 before:right-1 before:rounded-md "
+              >
+                <img src={notificationIcon} width={20} height={20} alt="" />
+              </div>
+
+              <div className=" font-semibold user-name">
+                {firstName} {lastName}
+              </div>
+
+              <div className=" bg-blue-300 rounded-full p-1">
+                {firstName.substring(0, 1)}
+                {lastName.substring(0, 1)}
+              </div>
+            </div>
+          )}
         </div>
       </header>
     </div>
