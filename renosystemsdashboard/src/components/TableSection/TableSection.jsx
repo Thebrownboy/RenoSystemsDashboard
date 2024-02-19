d728fe7c03e8beb8bdca1923c10eaa37761518c0
thebrownboy <abdallaxpmostafa@gmail.com>
2024-02-19 07:58:27 +0200

    refactor the users to be dynamic

--- renosystemsdashboard/src/components/TableSection/TableSection.jsx
+++ renosystemsdashboard/src/components/TableSection/TableSection.jsx
@@ -2,85 +2,27 @@ import React from "react";
 import TableHeader from "../TableHeader/TableHeader";
 import TableRow from "../TableRow/TableRow";
 import styles from "./tablesection.module.css";
+import { users } from "../../../constants";
 function TableSection() {
   return (
     <div
       className={`${styles["table-section"]} border-1 overflow-x-scroll border border-l-[transparent] border-r-[transparent] `}
     >
       <TableHeader />
-      <TableRow
-        firstName={"Ramy"}
-        lastName={"Mohsen"}
-        userName={"Ramy.Mohsen"}
-        createdOn={"Dec 10,2022"}
-        email={"ramy.mohsen@google.com"}
-        group={"Office"}
-        status={"locked"}
-      />
-
-      <TableRow
-        firstName={"Ramy"}
-        lastName={"Mohsen"}
-        userName={"Ramy.Mohsen"}
-        createdOn={"Dec 10,2022"}
-        email={"ramy.mohsen@google.com"}
-        group={"Office"}
-        status={"locked"}
-      />
-      <TableRow
-        firstName={"Ramy"}
-        lastName={"Mohsen"}
-        userName={"Ramy.Mohsen"}
-        createdOn={"Dec 10,2022"}
-        email={"ramy.mohsen@google.com"}
-        group={"Office"}
-        status={"locked"}
-      />
-      <TableRow
-        firstName={"Ramy"}
-        lastName={"Mohsen"}
-        userName={"Ramy.Mohsen"}
-        createdOn={"Dec 10,2022"}
-        email={"ramy.mohsen@google.com"}
-        group={"Office"}
-        status={"locked"}
-      />
-      <TableRow
-        firstName={"Ramy"}
-        lastName={"Mohsen"}
-        userName={"Ramy.Mohsen"}
-        createdOn={"Dec 10,2022"}
-        email={"ramy.mohsen@google.com"}
-        group={"Office"}
-        status={"locked"}
-      />
-      <TableRow
-        firstName={"Ramy"}
-        lastName={"Mohsen"}
-        userName={"Ramy.Mohsen"}
-        createdOn={"Dec 10,2022"}
-        email={"ramy.mohsen@google.com"}
-        group={"Office"}
-        status={"locked"}
-      />
-      <TableRow
-        firstName={"Ramy"}
-        lastName={"Mohsen"}
-        userName={"Ramy.Mohsen"}
-        createdOn={"Dec 10,2022"}
-        email={"ramy.mohsen@google.com"}
-        group={"Office"}
-        status={"locked"}
-      />
-      <TableRow
-        firstName={"كريم"}
-        lastName={"محسن"}
-        userName={"Ramy.Mohsen"}
-        createdOn={"Dec 10,2022"}
-        email={"ramy.mohsen@google.com"}
-        group={"Office"}
-        status={"locked"}
-      />
+      {users.map((item, index) => {
+        return (
+          <TableRow
+            createdOn={item.createdOn}
+            email={item.email}
+            firstName={item.firstName}
+            group={item.group}
+            lastName={item.lastName}
+            status={item.status}
+            userName={item.userName}
+            key={index}
+          />
+        );
+      })}
     </div>
   );
 }
